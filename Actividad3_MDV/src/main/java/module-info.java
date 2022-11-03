301d8e8a9b038615aea79d02a00252af50b6a973
mariadv_ <95478604+mariaduva@users.noreply.github.com>
2022-11-03 11:48:31 +0100

    changes

--- Actividad3_MDV/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.ej3.ejercicio3addressapp {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.ej3.ejercicio3addressapp to javafx.fxml;
-    exports com.ej3.ejercicio3addressapp;
-}
